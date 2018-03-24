4cef5e838434c0be0ef253a7d310457945eb207b
Ian Johnson <ian.johnson@canonical.com>
2018-03-23 20:03:52 -0500

    Make SHA3 functionality separate package from executable Now the actual SHA3 hashing functionality lives separately from the executable, and the executable uses that library. Also updating the CMake script that builds this to build the package separately using the updated script from golang_with_cmake.

--- cmd/sha3/main.go
+++ cmd/sha3/main.go
@@ -11,23 +11,10 @@ import (
 	"os"
 	"strings"
 	"time"
-	"unsafe"
 
 	// osutil "github.com/snapcore/snapd/osutil"
-	_ "golang.org/x/crypto/sha3"
-)
-
-// #cgo CFLAGS: -fno-pie -no-pie -I${LIBKECCAK_HEADERS}
-// #cgo LDFLAGS: -no-pie ${LIBKECCAK_LIBRARY}
-// #include <stdlib.h>
-// #include "KeccakHash.h"
-// #include "KeccakSpongeWidth1600.h"
-// #include "KeccakP-1600-SnP.h"
-import "C"
-
-// For FIPS202 standard
-const (
-	fipsDelimiter = 0x06
+	// _ "golang.org/x/crypto/sha3"
+	sha3 "github.com/anonymouse64/sha3_arm/sha3"
 )
 
 const (
@@ -48,100 +35,6 @@ func FileDigest(filename string, hash hash.Hash) ([]byte, uint64, error) {
 		return nil, 0, err
 	}
 	return hash.Sum(nil), uint64(size), nil
-}
-
-type Sha3FastHasher struct {
-	Rate            int
-	Capacity        int
-	Hashbitlen      int
-	DelimitedSuffix int
-	instance        C.Keccak_HashInstance
-	lasthashedbytes unsafe.Pointer
-}
-
-func NewKeccak512() *Sha3FastHasher {
-	hashers := make([]Sha3FastHasher, 1)
-	hashers[0] = Sha3FastHasher{
-		Rate:            576,
-		Capacity:        1024,
-		Hashbitlen:      512,
-		DelimitedSuffix: fipsDelimiter,
-	}
-
-	// If this fails we have to panic cause we can't return an error
-	if res := C.Keccak_HashInitialize(
-		&hashers[0].instance,
-		_Ctype_uint(hashers[0].Rate),
-		_Ctype_uint(hashers[0].Capacity),
-		_Ctype_uint(hashers[0].Hashbitlen),
-		_Ctype_uchar(hashers[0].DelimitedSuffix),
-	); res != 0 {
-		panic("failed to initialize hasher")
-	}
-	return &hashers[0]
-}
-
-// Write function absorbs bytes into the sponge
-func (h *Sha3FastHasher) Write(b []byte) (n int, err error) {
-	if h.lasthashedbytes != nil {
-		C.free(h.lasthashedbytes)
-	}
-	byteLen := C.size_t(len(b))
-	bytes := C.CBytes(b)
-	h.lasthashedbytes = bytes
-	res := C.Keccak_HashUpdate(
-		&h.instance,
-		(*_Ctype_uchar)(h.lasthashedbytes),
-		byteLen*8,
-	)
-	if res != 0 {
-		return 0, fmt.Errorf("failed to update hash")
-	}
-	return len(b), nil
-}
-
-// Sum appends the current hash to b and returns the resulting slice.
-// It does not change the underlying hash state.
-// TODO: currently this doesn't obey the second part of the function, i.e.
-// it does currently change the underlying hash state if b isn't nil
-func (h *Sha3FastHasher) Sum(b []byte) []byte {
-	// check if b is nil, in which case we just run on the state we have accumulated
-	if b != nil {
-		// need to duplicate the state and then write these bytes to it
-		// before summing it
-		h.Write(b)
-	}
-	// Allocate data for the output buffer - need to take the ceiling of the
-	// hashbitlen / 8 for the size of it
-	outputlen := (h.Hashbitlen + 7) / 8
-	// fmt.Printf("bytes addr: %#v\n", bytes)
-	res := C.Keccak_HashFinal(
-		&h.instance,
-		(*_Ctype_uchar)(h.lasthashedbytes),
-	)
-	if res != 0 {
-		return nil
-	}
-	// copy the c memory to go memory
-	return C.GoBytes(h.lasthashedbytes, _Ctype_int(outputlen))
-}
-
-// Reset resets the hasher's internal state to its initial state.
-func (h *Sha3FastHasher) Reset() {
-	// TODO: implement the state part of this
-}
-
-// Size returns the number of bytes Sum will return.
-func (h *Sha3FastHasher) Size() int {
-	return h.Hashbitlen / 8
-}
-
-// BlockSize returns the hash's underlying block size.
-// The Write method must be able to accept any amount
-// of data, but it may operate more efficiently if all writes
-// are a multiple of the block size.
-func (h *Sha3FastHasher) BlockSize() int {
-	return h.Rate / 8
 }
 
 func timeFileHash(hasher hash.Hash, file string) ([]byte, time.Duration) {
@@ -241,7 +134,7 @@ func main() {
 	var hashBytes []byte
 	var timeRes time.Duration
 	for i := 0; i < *numIters; i += 1 {
-		hasherToUse := NewKeccak512()
+		hasherToUse := sha3.NewKeccak512()
 		hashBytes, timeRes = timeFileHash(hasherToUse, *fileStr)
 		timeResults[i] = timeRes
 	}
