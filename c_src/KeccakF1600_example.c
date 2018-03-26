35089ff0abd974063b5559438e5f1052c6b9e39e
Ian Johnson <ian.johnson@canonical.com>
2018-03-26 11:40:56 -0500

    Add ARM assembly version of permute function
    This is to test the bare assembly permute function that is to be translated.

---
+++ c_src/KeccakF1600_example.c
@@ -0,0 +1,31 @@
+// Example executable of using the libkeccak wrapper library
+
+#include <stdint.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+
+// state must be 1600 bits - i.e. 25 uint64_t's
+extern void KeccakF1600( void *state ); 
+
+int main(int argc, char ** argv)
+{
+	uint64_t state[25];
+	for(int i = 0; i < 25; i++)
+	{
+		state[i] = i;
+	}
+
+	KeccakF1600((void*) state);
+
+	for(int i = 0; i < 25; i++)
+	{
+		printf("state[%d] = %" PRIu64 "\n",i, state[i]);
+	}
+	return 0;
+}
