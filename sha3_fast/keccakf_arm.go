0886398e3492034a4be6909f2b1f771f84b3203d
Ian Johnson <ian.johnson@canonical.com>
2018-03-26 18:26:44 -0500

    Change assembly function signature to uint64_t[]
    The assembly code doesn't care what the signature of the function is, so long as the first argument is a pointer to the start of the 1600 bits that make up the state. We can help cgo when calling the permute function and avoid having to copy data or cast anything as now the uint64[] in golang maps cleanly to a uint64_t[] (at least from cgo's perspective). This also means we don't have to copy the data from go to c before using it.

--- sha3_fast/keccakf_arm.go
+++ sha3_fast/keccakf_arm.go
@@ -6,15 +6,11 @@
 
 package sha3_fast
 
-// #cgo CFLAGS: -fno-pie -no-pie -I
-// #cgo LDFLAGS: -no-pie /home/ijohnson/git/canonical/sha3_arm/build/c_src
+// #cgo CFLAGS: -fno-pie -no-pie -I/home/pi/git/sha3_arm/c_src/include
+// #cgo LDFLAGS: -no-pie /home/pi/git/sha3_arm/build/c_src/libkeccakf1600.a
 // #include <stdlib.h>
 // #include "KeccakF1600ARM.h"
 import "C"
-
-import (
-	_ "unsafe"
-)
 
 // To detect what version of arm we are running on we need to get goarm from the runtime
 //go:linkname goarm runtime.goarm
@@ -24,12 +20,8 @@ var goarm uint8
 // generic implementation
 func keccakF1600(a *[25]uint64) {
 	if goarm > 7 {
-		C.KeccakF1600(a)
+		C.KeccakF1600((*_Ctype_ulonglong)(&a[0]))
 	} else {
 		keccakF1600Generic(a)
 	}
 }
-
-// These functions are implemented in keccakf_arm.s.
-//go:noescape
-func keccakF1600Neon(a *[25]uint64)
